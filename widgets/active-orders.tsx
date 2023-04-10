e43067be4e2e24a7b7a8e0610edfb024557aed40
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-04-10 15:21:36 -0700

    Update active-orders.tsx

--- widgets/active-orders.tsx
+++ widgets/active-orders.tsx
@@ -3,7 +3,7 @@ export const ActiveOrders = () => {
     <div className="w-full bg-white rounded py-2">
       <div className="flex flex-wrap items-center px-4 py-2">
         <div className="relative w-full max-w-full flex-grow flex-1">
-          <h3 className="font-semibold text-base text-gray-900">Active Orders</h3>
+          <h3 className="font-semibold text-base text-gray-900">...</h3>
         </div>
         <div className="relative w-full max-w-full flex-grow flex-1 text-right">
           <button className="bg-blue-500 text-white active:bg-blue-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">See all</button>
@@ -19,17 +19,17 @@ export const ActiveOrders = () => {
                 <ul className="rb">
                   <li className="rb-item" ng-repeat="itembx">
                     <a href="#" className="relative flex flex-row ">
-                      <span className="text-sm font-bold text-slate-600">Manama Bahrain</span>
+                      <span className="text-sm font-bold text-slate-600">...</span>
                       <span className="hidden md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-white bg-emerald-500 rounded-full">Out for delivery</span>
                     </a>
-                    <small className="text-xs  text-slate-500">3rd May 2020 7:00 PM</small>
+                    <small className="text-xs  text-slate-500">...</small>
                   </li>
 
                   <li className="rb-item" ng-repeat="itembx">
                     <a href="#" className="relative flex flex-row ">
-                      <span className="text-sm font-bold text-slate-600">Ilocos Norte Philippines</span>
+                      <span className="text-sm font-bold text-slate-600">...</span>
                     </a>
-                    <small className="text-xs text-slate-500">19th May 2020 3:00 PM</small>
+                    <small className="text-xs text-slate-500">...</small>
                   </li>
                 </ul>
               </div>
@@ -44,17 +44,17 @@ export const ActiveOrders = () => {
                 <ul className="rb">
                   <li className="rb-item" ng-repeat="itembx">
                     <a href="#" className="relative flex flex-row ">
-                      <span className="text-sm font-bold text-slate-600">GuangZhou China</span>
+                      <span className="text-sm font-bold text-slate-600">...</span>
                       <span className="hidden md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-slate-500 bg-slate-200 rounded-full">Warehouse</span>
                     </a>
-                    <small className="text-xs text-slate-500">3rd May 2020 7:00 PM</small>
+                    <small className="text-xs text-slate-500">...</small>
                   </li>
 
                   <li className="rb-item" ng-repeat="itembx">
                     <a href="#" className="relative flex flex-row ">
-                      <span className="text-sm font-bold text-slate-600">Dasmariñas Philippines</span>
+                      <span className="text-sm font-bold text-slate-600">...</span>
                     </a>
-                    <small className="text-xs text-slate-500">19th May 2020 3:00 PM</small>
+                    <small className="text-xs text-slate-500">...</small>
                   </li>
                 </ul>
               </div>
@@ -69,17 +69,17 @@ export const ActiveOrders = () => {
                 <ul className="rb">
                   <li className="rb-item" ng-repeat="itembx">
                     <a href="#" className="relative flex flex-row ">
-                      <span className="text-sm font-bold text-slate-600">Yiwu China</span>
+                      <span className="text-sm font-bold text-slate-600">...</span>
                       <span className="hidden md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-red-500 bg-red-50 rounded-full">In transit</span>
                     </a>
-                    <small className="text-xs  text-slate-500">3rd May 2020 7:00 PM</small>
+                    <small className="text-xs  text-slate-500">...</small>
                   </li>
 
                   <li className="rb-item" ng-repeat="itembx">
                     <a href="#" className="relative flex flex-row ">
-                      <span className="text-sm font-bold text-slate-600">Valenzuela Philippines</span>
+                      <span className="text-sm font-bold text-slate-600">...</span>
                     </a>
-                    <small className="text-xs text-slate-500">19th May 2020 3:00 PM</small>
+                    <small className="text-xs text-slate-500">...</small>
                   </li>
                 </ul>
               </div>
