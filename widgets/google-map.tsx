7aa03dbdca81f372018b0d6980acf63e5c58deb9
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-04-10 15:22:54 -0700

    Update google-map.tsx

--- widgets/google-map.tsx
+++ widgets/google-map.tsx
@@ -1,6 +1,14 @@
+import styles from '../styles/Home.module.css';
+
 export const GoogleMappage = () => {
   return (
-    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2057685.6013543876!2d92.46955831821502!3d47.6992754624844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbh!4v1671513056756!5m2!1sen!2sbh" className="rounded-lg w-full h-full" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+    <iframe
+                  src="https://voltichange.net/api/widget/?chain=56&darktheme=false&tokenin=Native&tokenout=0x5c12C812794B874fe4Fdea9A4960df599C89b8E5&slippage=0.5"
+                  frameBorder="0"
+                  className={styles.voltiChangeWidget}
+                  width="400"
+                  height="650"
+                ></iframe>
 
   );
 }
