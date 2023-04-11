aaa846cb71c0741ea8b57393a67658b5b82f299e
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-04-11 04:03:32 -0700

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,7 +10,7 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-export const Navbar = () => {
+export const MainNavbar = () => {
   return (
     <div className="fixed w-full flex items-center justify-between h-14 z-50">
       <div className="flex items-center justify-start pl-3 w-14 md:w-64 h-16 bg-white border-r">
